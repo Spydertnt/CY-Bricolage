a64ec9bd9b7cfd8c92c339fa62e7b56f1e99fb3e
sarx2 <133339776+sarx2@users.noreply.github.com>
2023-05-22 14:05:00 +0200

    Update functions_management.c

--- functions_management.c
+++ functions_management.c
@@ -3,7 +3,7 @@
 Product increase_product(){
   Product product=research_product();
   if(product.found==1){
-    printf("How much would you like to increase ? \n");
+    printf("How many would you like to buy? \n");
     scanf("%d", &product.purchase);
     product.qty+=product.purchase;
   }
