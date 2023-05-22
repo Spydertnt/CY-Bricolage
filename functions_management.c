20b11a14cf6e527f276c28f127a5656f097762ca
louevaberanger <133343765+louevaberanger@users.noreply.github.com>
2023-05-22 15:28:30 +0200

    Update functions_management.c secure

--- functions_management.c
+++ functions_management.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
+
+
 Product increase_product(){
   Product product=research_product();
+  int verif;
   if(product.found==1){
-    printf("How many would you like to buy? \n");
-    scanf("%d", &product.purchase);
-    product.qty+=product.purchase;
-  }
+  do{
+    printf("How much would you like to increase ? \n");
+    verif = scanf("%d", &product.purchase);
+    vide_buffer();
+  }while(verif!=1);
+  product.qty+=product.purchase;
   /*else{
     printf("Product does not exist or return\n");
   }*/
   return product;
+}
 }
