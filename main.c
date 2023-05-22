db5b824f9105368ae72dbeb39d4c5a703765de0b
louevaberanger <133343765+louevaberanger@users.noreply.github.com>
2023-05-22 15:24:01 +0200

    Update main.c secure

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+
 int main() {
 
   int mode;
+  int verif;
  
   printf("-------Welcome to Castorama------- \n ");
   do{
     printf("Which mode would you like to access ? \n");
     printf("\n  1- Management mode");
     printf("\n  2- Purchase mode \n");
-    scanf("%d", &mode);
-  }while(mode!=1 && mode!=2);
+    verif = scanf("%d", &mode);
+    vide_buffer();
+  }while(mode!=1 && mode!=2 || verif!=1);
 
   if(mode==1){
     management();
