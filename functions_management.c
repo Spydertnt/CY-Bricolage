2e0dd329856f76dc8ab7c96984cc50cc1b397200
louevaberanger <133343765+louevaberanger@users.noreply.github.com>
2023-05-26 18:32:14 +0200

    Update functions_management.c commentaires

--- functions_management.c
+++ functions_management.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 Product increase_product(){
+  //recherche du produit
   Product product=research_product();
   int verif;
+  //si le produit est trouvé :
   if(product.found==1){
   do{
     printf("How much would you like to increase ? \n");
     verif = scanf("%d", &product.purchase);
+    //on vide la mémoire tampon
     vide_buffer();
   }while(verif!=1);
+  //on augmente la quantité du produit de la quantité souhaitée
   product.qty+=product.purchase;
   /*else{
     printf("Product does not exist or return\n");
