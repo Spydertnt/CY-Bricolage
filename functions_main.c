5eb3323c54fb820d43cacdb3218f1652561c4a32
Lucas Gournay <133283871+Spydertnt@users.noreply.github.com>
2023-05-27 20:55:24 +0200

    Update functions_main.c

--- functions_main.c
+++ functions_main.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
+//Sécurise les printf
 void vide_buffer(){
 	while(getchar()!='\n'){
 	}
 }
 
+//Fonction de recherche de produit
 Product research_product(){
-  char name[20];
-  int ref, verif;
+  
+  //Déclaration et initialisation des variables à 0
+  char name[20] = {0};
+  int ref = 0, verif = 0, rech = 0;
   Product product={0}, product2={0};
-  int rech;
-  int currentCaract;
   FILE* file;
-  //on ouvre le fichier products en mode lecture
+  
+  //On ouvre le fichier products en mode lecture
   file = fopen("products.txt", "r");
+
+  //On vérifie que le fichier existe bien sinon on renvoie une erreur
   if(file==NULL){
     printf("Cannot open file \n");
     exit(1);
   }
+
+  //Quel est le critère de recherche
   do{
   printf("\n       1- Search by ref:");
   printf("\n       2- Search by name: ");
@@ -26,124 +33,180 @@ Product research_product(){
   verif  = scanf("%d" , &rech);
   vide_buffer();
   }while(verif!=1 || rech!=1 && rech!=2 && rech!=3);
-//on remet le curseur au début dans le fichier produit 
+
+  //On remet le curseur au début dans le fichier produit 
   rewind(file);
-//switch case en fonction de la réponse de l'utilisateur
-    switch(rech){
-      case 1:do{
+
+  //Switch case en fonction de la réponse de l'utilisateur
+  switch(rech){
+    //Recherche avec la référence du produit
+    case 1: do{
       printf("       Enter product's ref:");
       verif = scanf("%d", &ref);
       vide_buffer();
-      }while(verif!=1);
-	//lecture du fichier products, tant que le fichier récupère 5 valeurs correctes
-          while(fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size)==5){
-            product.line++;
-	    //si la réf correspond à une ref du fichier products
-            if(ref==product.ref){
-              product.found = 1;
-              break;
-          }
-        }
+    }while(verif!=1);
+      
+    //Lecture du fichier products, tant que le fichier récupère 5 valeurs correctes
+    while(fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size)==5){
+      //On incrémente la ligne du produit
+      product.line++;
+	    //Si la réf saisie correspond à une réf du fichier products, le produit est indiqué comme trouvé
+      if(ref==product.ref){
+        product.found = 1;
         break;
-      case 2: do{
+      }
+    }
+      
+    break;
+
+    //Recherche par nom
+    case 2: do{
       printf("      Enter product's name :");
       verif = scanf("%s",name);
       vide_buffer();
-      }while(verif!=1);
-	  //lecture du fichier products, tant que le fichier récupère 5 valeurs correctes
-          while(fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size)==5){
-	    //on incrémente la ligne du produit
-            product.line++;
-	    //si le nom correspond à un nom de produits du fichier 
-            if(strcmp(product.name,name)==0){
-              product.found = 1;
-              break;
-          }
-        }
+    }while(verif!=1);
+      
+	  //Lecture du fichier products, tant que le fichier récupère 5 valeurs correctes
+    while(fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size)==5){
+	    //On incrémente la ligne du produit
+      product.line++;
+	    //Si le nom saisie est identique au nom d'un des produits écrit dans le fichier, le produit est indiqué comme trouvé 
+      if(strcmp(product.name,name)==0){
+        product.found = 1;
         break;
-      case 3:
-        fclose(file);
+      }
+    }
+      
+    break;
+    
+    case 3:
+      fclose(file);
+
+    //Message d'au revoir
   	printf("╔═════════════════════════════════════╗\n");
   	printf("║        SEE YOU SOON AT CASTO        ║\n");
   	printf("╚═════════════════════════════════════╝\n");
         exit(4);
-    }
+  }
 
+  //Fermeture du fichier products.txt
   fclose(file);
- //si le produit recherché est bien dans le fichier produits, on affiche la ligne du produit correspondant 
+  
+  //Si le produit recherché est bien dans le fichier produits, on affiche la ligne du produit correspondant 
   if(product.found==1){
     printf("\nName\tRef\tQuantity\tPrice\tSize");
     printf("\n-----------------------------------");
     printf("\n%s\t%d\t%d\t%d\t%d\n", product.name, product.ref, product.qty, product.price, product.size);
     return product;
   }
-//sinon on quitte la fonction
+  //Sinon on quitte la fonction
   else{
     product.found=0;
     printf("Product does not exist\n");
     return product2;
   }
-
+  
 }
 
 int calculate_stock(){
+  
+  //Déclaration et initialisation des variables à 0
   int stock_used = 0;
   Product product={0};
   FILE* file = NULL;
   FILE* file2 = NULL;
-//ouverture du fichier produits en mode lecture
+  
+  //Ouverture du fichier produits en mode lecture
   file = fopen("products.txt", "r");
-//ouverture du fichier stock en mode lecture écriture
+
+  //On vérifie que le fichier existe bien sinon on renvoie une erreur
+  if(file==NULL){
+    printf("Cannot open file \n");
+    exit(10);
+  }
+  
+  //Ouverture du fichier stock en mode lecture écriture
   file2 = fopen("stock.txt", "r+");
-//lecture du fichier produits
+
+  //On vérifie que le fichier existe bien sinon on renvoie une erreur
+  if(file2==NULL){
+    printf("Cannot open file \n");
+    exit(11);
+  }
+  
+  //Lecture du fichier produits
   for(int i=0; i<nbr_products; i++){
     fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size);
-//on incrémente stock_used de la quantité du produit par sa taille
+    //On incrémente stock_used de la quantité du produit multiplié par sa taille
     stock_used+=(product.qty*product.size);
   }
-//on réécrit dans le fichier stock la nouvelle valeur du stock restant, c'est à dire 1000-stock_used
-    fprintf(file2, "%d %d", shop_stock, 1000-stock_used);
-//fermeture des fichiers
-    fclose(file);
-    fclose(file2);
-    return stock_used;
+  
+  //On réécrit dans le fichier stock la nouvelle valeur du stock restant, c'est à dire 1000-stock_used
+  fprintf(file2, "%d %d", shop_stock, 1000-stock_used);
+  //Fermeture des fichiers
+  fclose(file);
+  fclose(file2);
+
+  //On retourne le stock utilisé
+  return stock_used;
+  
 }
 
 void rewrite(Product increase){
-  int position=0;
+
+  //Déclaration et initialisation des variables
+  int position = 0;
   Product product, inventory[nbr_products];
-//ouverture du fichier produits en mode lecture écriture
+  char line[200];
+  
+  //Ouverture du fichier produits en mode lecture écriture
   FILE* file = NULL;
   file = fopen("products.txt","r+");
-//ouverture d'un fichier temporaire en mode lecture écriture
+
+  //On vérifie que le fichier existe bien sinon on renvoie une erreur
+  if(file==NULL){
+    printf("Cannot open file \n");
+    exit(12);
+  }
+  
+  //Ouverture d'un fichier temporaire en mode lecture écriture
   FILE* temp = NULL;
   temp = fopen("temp.txt","w+");
-  char line[200];
-//on lit le fichier produits, jusqu'à la ligne du produit qui est augmenté
+  
+  //On lit le fichier produits, jusqu'à la ligne du produit qui est augmenté
   for(int i=1; i<increase.line; i++){
     fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size);
-//on ecrit dans le fichier temporaire les lignes des produits qui ne sont pas modifés
+    //On copie dans le fichier temporaire les lignes des produits qui ne sont pas modifés
     fprintf(temp, "%s %d %d %d %d\n", product.name, product.ref, product.qty, product.price, product.size);
+    //Synchronisation du buffer en mémoire avec le fichier
     fflush(temp);
-    
   }
-//si la ligne du produit est différente de 1, on écrit un saut de ligne dans le fichier temporaire
+  
+  //Si la ligne du produit est différente de 1, on écrit un saut de ligne dans le fichier temporaire
   if(increase.line!=1){
     fprintf(file, "\n");
   }
-//on recopie à la suite du fichier temp la ligne du produit qui est modifié
+  
+  //On recopie à la suite du fichier temp la ligne du produit qui est modifié
   fprintf(temp, "%s %d %d %d %d\n", increase.name, increase.ref, increase.qty, increase.price, increase.size);
+
+  //On copie dans un tableau la ligne originale du produit qui ne sert plus pour la passer 
   fgets(line, 199, file);
-//après cette ligne modifiée, on recopie les autres lignes des produits d'après qui ne changent pas
+  
+  //Après cette ligne modifiée, on recopie les autres lignes des produits d'après qui ne changent pas
   for(int i=increase.line; i<nbr_products; i++){
     fscanf(file, "%s %d %d %d %d", product.name, &product.ref, &product.qty, &product.price, &product.size);
     fprintf(temp, "%s %d %d %d %d\n", product.name, product.ref, product.qty, product.price, product.size);
     fflush(temp);
   }
-//on supprime le fichier products.txt
+  
+  //On supprime l'ancien fichier products.txt
   remove("products.txt");
-//on renomme le fichier temporaire en "products.txt"
+  
+  //On renomme le fichier temporaire en "products.txt"
   rename("temp.txt", "products.txt");
-//affichage du stock restant 
+  
+  //Affichage du stock restant 
   printf("The remaining stock is : %d\n", shop_stock-calculate_stock());
+  
 }
